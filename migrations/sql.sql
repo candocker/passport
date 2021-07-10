29c35bc7cff45cd19096d88ebb3b91766355fed3
acanhost <acanstudio@canliang.wang>
2021-07-10 10:53:02 +0800

    develop passport

---
+++ migrations/sql.sql
@@ -0,0 +1,13 @@
+REPLACE INTO `wp_auth_role_permission`(`role_code`, `permission_code`) SELECT 'admin', `code` FROM `wp_auth_permission` WHERE 1 ;
+INSERT INTO `wp_auth_role_permission`(`role_code`, `permission_code`) SELECT 'editor', `code` FROM `wp_auth_permission` WHERE `app` IN ('merchant') ;
+
+
+-------------------旧数据-------------
+INSERT INTO `wp_auth_permission` (`code`, `resource_code`, `parent_code`, `name`, `app`, `controller`, `action`, `orderlist`, `display`, `extparam`) SELECT `code`, `elem_code`, `parent_code`, `name`, `module`, `controller`, `method`, `orderlist`, `display`, `extparam` FROM `bak_passport`.`wp_auth_permission0808` WHERE `module` = 'merchant' ;
+UPDATE `wp_auth_permission` SET `method` = 'get' WHERE `app` IN ('culture') AND `method` = '' AND `action` IN ('listinfo', 'view');
+UPDATE `wp_auth_permission` SET `method` = 'post' WHERE `app` IN ('culture') AND `method` = '' AND `action` IN ('add');
+UPDATE `wp_auth_permission` SET `method` = 'put' WHERE `app` IN ('culture') AND `method` = '' AND `action` IN ('update');
+UPDATE `wp_auth_permission` SET `method` = 'delete' WHERE `app` IN ('culture') AND `method` = '' AND `action` IN ('delete');
+UPDATE `wp_auth_permission` SET `method` = 'get' WHERE `app` IN ('culture') AND `method` = '' AND `action` != '';
+
+
