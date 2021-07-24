95aeadefdcca5925652957d2a90186f965c82b0b
acanhost <acanstudio@canliang.wang>
2021-07-24 09:38:01 +0800

    develop main

--- app/Resources/InfoAttachmentCollection.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace ModulePassport\Resources;
-
-use Framework\Baseapp\Resources\AbstractCollection;
-
-class InfoAttachmentCollection extends AbstractCollection
-{
-}
--- app/Resources/RegionCollection.php
+++ app/Resources/RegionCollection.php
@@ -2,8 +2,6 @@
 
 namespace ModulePassport\Resources;
 
-use Framework\Baseapp\Resources\AbstractCollection;
-
 class RegionCollection extends AbstractCollection
 {
 }
